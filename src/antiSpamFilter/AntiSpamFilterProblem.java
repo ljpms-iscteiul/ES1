d7211f76f3815825b831e437b56f67e30152b44c
carlossaraiva <carlossaraiva@192.168.1.101>
2017-11-21 12:19:26 +0000

    System outprints para perceber como funcionam as classes do stor

--- src/antiSpamFilter/AntiSpamFilterProblem.java
+++ src/antiSpamFilter/AntiSpamFilterProblem.java
@@ -36,16 +36,19 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 	    double[] x = new double[getNumberOfVariables()];
 	    for (int i = 0; i < solution.getNumberOfVariables(); i++) {
 	      x[i] = solution.getVariableValue(i) ;
+	      System.out.println("Este é o xi"+ x[i]);
 	    }
 
 	    fx[0] = 0.0;
 	    for (int var = 0; var < solution.getNumberOfVariables() - 1; var++) {
 		  fx[0] += Math.abs(x[0]); // Example for testing
+		  System.out.println("Este é o fx0"+ fx[0]);
 	    }
 	    
 	    fx[1] = 0.0;
 	    for (int var = 0; var < solution.getNumberOfVariables(); var++) {
 	    	fx[1] += Math.abs(x[1]); // Example for testing
+	    	System.out.println("Este é o fx1"+ fx[1]);
 	    }
 
 	    solution.setObjective(0, fx[0]);

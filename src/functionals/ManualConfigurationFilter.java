1bba7425e2ace099c1122760b03050e6af034408
carlossaraiva <carlossaraiva@mbp-de-carlos.lan>
2017-11-22 23:57:34 +0000

    olh olha

--- src/functionals/ManualConfigurationFilter.java
+++ src/functionals/ManualConfigurationFilter.java
@@ -7,25 +7,36 @@ public class ManualConfigurationFilter {
 		ManualConfigurationFilter novo= new ManualConfigurationFilter();
 		double fp= novo.falsospositivospercent();
 		System.out.println("Este é o fp"+ fp);
-		double fn = novo.falsosnegativospercent();
-		System.out.println("Este é o fn"+fn);
+		//double fn = novo.falsosnegativospercent();
+		//System.out.println("Este é o fn"+fn);
 		
 	}
+	
+	//falsos positivos é quando no ficheiro 
 	
 	public int CalculoFalsosPositivos(){
 		
 		HamSpamReader calculator= new HamSpamReader();
-		double[] ham= calculator.WeigthCalculator("ham.log");
+		double[] spam= calculator.WeigthCalculator("ham.log");
 		int falsospositivos = 0;
-		System.out.println("Entrou no calculo");
 		
-		for(int i=0;ham.length<i;i++) {
-			if(ham[i]<5)
+		for(int i=0;spam.length<i;i++) {
+			if(spam[i]<5)
+			System.out.println(falsospositivos);	
 			falsospositivos++;
 			
 		}
 		System.out.println("Este é o numero de falsos positivos "+ falsospositivos);
 		return falsospositivos;
+	}
+	
+	public double falsospositivospercent() {
+		LineCounter counter= new LineCounter();
+		int totallinhas= counter.LineCounterFunction("ham.log");
+		System.out.println(totallinhas);
+		double falsospositivos= CalculoFalsosPositivos()/totallinhas;
+		return falsospositivos;
+		
 	}
 	
 	public int CalculoFalsosNegativos(){
@@ -45,18 +56,10 @@ public class ManualConfigurationFilter {
 		
 	}
 	
-	public double falsospositivospercent() {
-		LineCounter counter= new LineCounter();
-		int totallinhas= counter.LineCounterFunction("ham.log");
-
-		double falsospositivos= CalculoFalsosPositivos()/totallinhas;
-		return falsospositivos;
-		
-	}
 	public double falsosnegativospercent() {
 		LineCounter counter= new LineCounter();
 		int totallinhas= counter.LineCounterFunction("spam.log");
-		
+		System.out.println(totallinhas);
 		double falsosnegativos= CalculoFalsosNegativos()/totallinhas;
 		return falsosnegativos;
 		
